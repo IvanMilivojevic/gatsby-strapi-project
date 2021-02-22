ab9880f6b3dbb7222c140d62f2302b4099d93449
Ivan Milivojevic <seltik89@hotmail.com>
2021-02-22 11:59:31 +0100

    seo refactor

--- src/layouts/Seo/Seo.js
+++ src/layouts/Seo/Seo.js
@@ -1,8 +1,10 @@
 import React from "react"
 import { Helmet } from "react-helmet"
+import { useLocation } from "@reach/router"
 import { useStaticQuery, graphql } from "gatsby"
 
-const SEO = ({ title, description }) => {
+const SEO = ({ title, description, image, article }) => {
+  const { pathname } = useLocation()
   const data = useStaticQuery(graphql`
     {
       site {
@@ -23,11 +25,29 @@ const SEO = ({ title, description }) => {
     },
   } = data
 
+  const seo = {
+    title: title ? `${title} | ${siteTitle}` : siteTitle,
+    description: description || siteDesc,
+    image: `${siteUrl}${image || siteImage}`,
+    url: `${siteUrl}${pathname}`,
+  }
+
   return (
-    <Helmet htmlAttributes={{ lang: "en" }}>
-      <title>{title ? `${title} | ${siteTitle}` : siteTitle}</title>
-      <meta name="description" content={description || siteDesc} />
-      <meta name="image" content={`${siteUrl}${siteImage}`} />
+    <Helmet title={seo.title} htmlAttributes={{ lang: "en" }}>
+      <meta name="description" content={seo.description} />
+      <meta name="image" content={seo.image} />
+
+      {seo.url && <meta property="og:url" content={seo.url} />}
+      {(article ? true : null) && <meta property="og:type" content="article" />}
+      {seo.title && <meta property="og:title" content={seo.title} />}
+      {seo.description && <meta property="og:description" content={seo.description} />}
+      {seo.image && <meta property="og:image" content={seo.image} />}
+
+      <meta name="twitter:card" content="summary_large_image" />
+      {/* {twitterUsername && <meta name="twitter:creator" content={twitterUsername} />} */}
+      {seo.title && <meta name="twitter:title" content={seo.title} />}
+      {seo.description && <meta name="twitter:description" content={seo.description} />}
+      {seo.image && <meta name="twitter:image" content={seo.image} />}
     </Helmet>
   )
 }
